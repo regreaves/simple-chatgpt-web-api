9d2e41b1136ffe7fe518606cd02b90db185de268
Ralph Greaves <rgreaves@ycp.edu>
2024-05-21 22:30:36 -0400

    Update openAIPlugin.mjs

--- openAIPlugin.mjs
+++ openAIPlugin.mjs
@@ -3,20 +3,20 @@ import OpenAI from 'openai';
 
 const createOpenAIInstance = apiKey => new OpenAI({ apiKey });
 
-const generateCompletion = openai => async ({ query }) => {
+const generateCompletion = (openai, model) => async ({ query }) => {
   const defaultMessages = [{ role: 'system', content: 'You are a helpful assistant.' }];
   const finalMessages = [...defaultMessages, { role: 'user', content: query }];
   const completion = await openai.chat.completions.create({
     messages: finalMessages,
-    model: 'gpt-3.5-turbo',
+    model: model,
   });
   return completion.choices[0]?.message?.content;
 };
 
 async function openAIPlugin(fastify, options) {
-  const { apiKey } = options;
+  const { apiKey, model = 'gpt-3.5-turbo' } = options;
   const openai = createOpenAIInstance(apiKey);
-  const chatGPT = generateCompletion(openai);
+  const chatGPT = generateCompletion(openai, model);
 
   fastify.decorate('queryChatGPT', chatGPT);
 }
